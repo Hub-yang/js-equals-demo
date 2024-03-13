8784c0bdc7cca74d92dfb7f7f4589ad30d230725
Hub-yang <18830279823@163.com>
2024-03-13 11:26:44 +0800

    chore: init

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+import VueRouter from 'unplugin-vue-router/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
+import UnoCSS from 'unocss/vite'
+
+export default defineConfig({
+  plugins: [
+    VueRouter(),
+    Vue(),
+    AutoImport({
+      imports: [
+        'vue',
+        '@vueuse/core',
+        VueRouterAutoImports,
+        {
+          // add any other imports you were relying on
+          'vue-router/auto': ['useLink'],
+        },
+      ],
+      dts: true,
+      dirs: [
+        './src/composables',
+      ],
+      vueTemplate: true,
+    }),
+    Components({
+      dts: true,
+    }),
+    UnoCSS(),
+  ],
+})
